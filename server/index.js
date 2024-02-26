cf5ed3f9d546fcdd4e30715b7ef1f60e0ae44525
Sanjayzng <106653066+Sanjayzng@users.noreply.github.com>
2024-02-26 18:24:54 +0530

    vite fix

--- server/index.js
+++ server/index.js
@@ -28,19 +28,17 @@ app.use("/api/message", messageRoute);
 // ------------------------------Deployment----------------------------------
 
 const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/dist")));
+app.use(express.static(path.join(__dirname1, "/client/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Welcome");
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"));
+});
 
 // ------------------------------Deployment----------------------------------
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome");
+// });
 
 app.listen(port, () => {
   console.log("Server listening on PORT: " + port);
